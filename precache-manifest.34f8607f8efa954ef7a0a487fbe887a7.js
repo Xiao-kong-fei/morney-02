320c43c465152cf328845b23628eaa446bf7ac22
xiaoxiao <3147264966@qq.com>
2022-06-17 02:21:18 +0800

    deploy

---
+++ precache-manifest.34f8607f8efa954ef7a0a487fbe887a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f34e0b390147dbbe1c",
+    "url": "/morney-02/css/app.6308016a.css"
+  },
+  {
+    "revision": "d4eb7983af72651f08926295aa74e39b",
+    "url": "/morney-02/index.html"
+  },
+  {
+    "revision": "64f34e0b390147dbbe1c",
+    "url": "/morney-02/js/app.7ad9e108.js"
+  },
+  {
+    "revision": "a633d06c973f6140e49c",
+    "url": "/morney-02/js/chunk-vendors.dc177a83.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-02/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-02/robots.txt"
+  }
+]);
